8511b33b2c09764ccbe5dafa6610b4d7b239ed12
juespega <juespega666@gmail.com>
2025-08-10 23:53:53 -0500

    Agregar archivos fp_clase03.py y fp_clase04.py con ejemplos de manejo de cadenas y funciones de impresión

---
+++ fp_clase03.py
@@ -0,0 +1,6 @@
+# Variables
+saludo_1 = "Hola"
+saludo_2 = "Hi"
+nombre = "Julián"
+print(saludo_1)
+print(nombre)
---
+++ fp_clase04.py
@@ -0,0 +1,23 @@
+name = "Julián"
+print(type(name))
+
+name = 'JULIAN Esteban'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-1])
+print(name[-2])
+
+last_name = "   Pérez   "
+print(name)
+print(last_name)
+print(name + " " + last_name)
+
+print(len(name))
+print(len(last_name))
+
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
